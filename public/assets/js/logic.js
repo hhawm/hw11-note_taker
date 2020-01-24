67e0fa84889875cd5809e067b228630c83aba268
henry hawm <henry.hawm@gmail.com>
2020-01-23 22:22:45 -0600

    starting

---
+++ public/assets/js/logic.js
@@ -0,0 +1,27 @@
+const todolistUl = $("#todolist");
+const newTodoList =  
+const newTodoButton =
+    $.get("/api/todos").then(data => {
+        console.log(data);
+        renderTodos(data);
+        data.forEach(todo =>
+            todolistUl.append(`
+        <li>${todo.text})</li>
+        `)
+        )
+    })
+
+newTodoButton.click(e => {
+    e.preventDefault();
+    const userText = newTodoInput.val();
+    console.log(userText);
+    const newTodo = {
+        text: userText,
+        done: false
+    };
+    $.post("/api/todos", newTodo).then(() => {
+        renderTodos(data);
+    }
+
+    )
+});
