02bd03d53cd64515121a78e8e613b8ce13967535
henry hawm <henry.hawm@gmail.com>
2020-01-28 01:52:08 -0600

    think its workin?

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
+const notes = require("./db/db.json");
 
 // Sets up the Express App
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const todos = [
-  {
-    text: "walk the dog",
-    done: false,
-  },
-  {
-    text: "wash the car",
-    done: true,
-  }
-];
-
+// Grabs HTML files in public folder
 app.use(express.static("public"));
 // Sets up the Express app to handle data parsing (middle ware)
 app.use(express.urlencoded({ extended: true }));
@@ -24,17 +15,36 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-app.get("/api/todos", function (req, res) {
-  return res.json(todos);
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
-// Create New ToDo input
-app.post("/api/todos", function (req, res) {
+
+
+// Create new note input
+app.post("/api/notes", function (req, res) {
   console.log(req.body);
-  todos.push(req.body);
-  res.send(200);
+  let note = req.body;
+  note.id = note.title.replace(/\s+/g, "").toLowerCase();
+  notes.push(note);
+  console.log(notes);
+});
+
+// Displays all current notes
+app.get("/api/notes", function (req, res) {
+  return res.json(notes);
+});
+
+// Deletes selected note
+app.delete("/api/notes/:note", function (req, res) {
+  let deletedNote = req.params.note;
+  for (let i = 0; i < notes.length; i++) {
+    if (deletedNote === notes[i].id) {
+      res.json(notes.splice(i, 1));
+    }
+  }
 });
 
 app.listen(PORT, function () {
@@ -46,54 +56,6 @@ app.listen(PORT, function () {
 
 
 
-// Routes
+
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
-});
-
-app.get("/add", function (req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
-});
-
-// Displays all characters
-app.get("/api/characters", function (req, res) {
-  return res.json(characters);
-});
-
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function (req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function (req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newCharacter = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newCharacter);
-
-  characters.push(newCharacter);
-
-  res.json(newCharacter);
-});
-
-// Starts the server to begin listening
-// =============================================================
